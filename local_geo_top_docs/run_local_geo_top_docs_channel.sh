1653925cc27c140a660d94bee6fbdbb5680c7750
tangyuyang <yuyang.tang@newsbreak.com>
2023-03-10 11:11:36 +0800

    mod: add more geo_new_docs

--- local_geo_top_docs/run_local_geo_top_docs_channel.sh
+++ local_geo_top_docs/run_local_geo_top_docs_channel.sh
@@ -9,22 +9,23 @@ PYTHON_BIN="/home/services/miniconda3/bin/python3"
 TIME_TAG=`date +%Y%m%d%H`
 
 ##### task conf
-TASK_NAME="local_geo_new_docs_second_cat"
+TASK_NAME="local_geo_top_docs"
 
 HDFS_PATH="s3a://pm-hdfs2/user/tangyuyang/${TASK_NAME}"
 LOCAL_DATA="/data/tangyuyang/${TASK_NAME}/data"
 LOCAL_LOG="/data/tangyuyang/${TASK_NAME}/log"
 
 # redis
-REDIS_PREFIX="local_geo_new"
+REDIS_PREFIX="local_geo_top"
 REDIS_TTL_SEC=86400
 
 # sql
-DOC_DATE_DIFF=1
-DOC_HOUR_DIFF=6
-DOC_SDATE=`date +"%Y-%m-%d" -d "-${DOC_DATE_DIFF} days"`
-DOC_SHOUR=`date +"%Y-%m-%d %H:00:00" -d "-${DOC_HOUR_DIFF} hours"`
-MAX_DOC_IN_GEO=20   # 单个key保存的结果数
+CJV_DATE_DIFF=3
+DOC_DATE_DIFF=3
+CJV_SDATE=`date +%Y-%m-%d -d "-${CJV_DATE_DIFF} days"`
+DOC_SDATE=`date +%Y-%m-%d -d "-${DOC_DATE_DIFF} days"`
+MIN_CHECK=100
+MAX_DOC_IN_GEO=200   # 单个key保存的结果数
 
 CLEAR_DAY=30
 MIN_RESULT_NUM=100
@@ -33,43 +34,71 @@ MIN_RESULT_NUM=100
 function get_docs() {
     local hdfs_cjv_path=${HDFS_PATH}"/data_${TIME_TAG}"
     local merged_file=${LOCAL_DATA}/${TIME_TAG}_data
+    local kv_file=${LOCAL_DATA}/${TIME_TAG}_kv
     local hive_sql="
 WITH geo_doc AS (
   SELECT
-    second_cat,
+    channel,
     concat(exid.type, '@', exid.pid) as type_pid,
-    concat(second_cat, '@', concat(exid.type, '@', exid.pid)) as cate_pid,
+    concat(channel, '@', concat(exid.type, '@', exid.pid)) as cate_pid,
     doc.doc_id,
     doc.publish_time
   FROM dim.document_parquet doc
-  LATERAL VIEW explode(text_category.second_cat) tmpTable AS second_cat, second_cat_score
   LATERAL VIEW explode(geotag) idtable as exid
+  LATERAL VIEW explode(channels) tmpTable AS channel
   WHERE doc.pdate >= '${DOC_SDATE}'
-    AND doc.publish_time >= '${DOC_SHOUR}'
-    AND second_cat IS NOT NULL
+    AND channel IS NOT NULL
     AND exid.pid IS NOT NULL
     AND exid.type != 'state'
 )
 
-, order_doc AS (
+, cate_geo_cjv AS (
   SELECT
-    cate_pid,
-    doc_id,
-    publish_time,
-    row_number() over(PARTITION BY cate_pid ORDER BY publish_time DESC) rk
-  FROM geo_doc
-)
+      cate_pid,
 
+      round(1.0000 * sum(cjv.clicked) / sum(cjv.checked), 4) as ctr,
+      sum(cjv.checked) as check,
+      sum(cjv.clicked) as click
+    FROM warehouse.online_cjv_hourly cjv
+    LEFT JOIN geo_doc ON geo_doc.doc_id = cjv.doc_id
+    WHERE
+      cjv.joined = 1
+      AND cjv.pdate >= '${CJV_SDATE}'
+      AND cjv.channel_name in ('foryou', 'local')
+      AND (cjv.nr_condition rlike 'local')
+      AND cjv.checked = 1
+      AND cate_pid IS NOT NULL
+    GROUP BY cate_pid
+)
 
 insert overwrite directory '${hdfs_cjv_path}' row format delimited fields terminated by '\t'
 
 SELECT
-  cate_pid,
-  concat_ws('#', collect_set(doc_id)) as docs
-FROM order_doc
-WHERE rk < ${MAX_DOC_IN_GEO}
-  AND cate_pid IS NOT NULL
-GROUP BY cate_pid
+  *
+FROM (
+    SELECT
+      cate_pid,
+      cjv.doc_id,
+
+      round(1.0000 * sum(cjv.clicked) / sum(cjv.checked), 4) as ctr,
+      sum(cjv.checked) as check,
+      sum(cjv.clicked) as click
+    FROM warehouse.online_cjv_hourly cjv
+    LEFT JOIN geo_doc ON geo_doc.doc_id = cjv.doc_id
+    WHERE
+      cjv.joined = 1
+      AND cjv.pdate >= '${CJV_SDATE}'
+      AND cjv.channel_name in ('foryou', 'local')
+      AND (cjv.nr_condition rlike 'local')
+      AND cjv.checked = 1
+      AND cate_pid IS NOT NULL
+    GROUP BY cate_pid, cjv.doc_id
+) t
+LEFT JOIN cate_geo_cjv ON cate_geo_cjv.cate_pid = t.cate_pid
+WHERE t.check >= ${MIN_CHECK}
+  AND t.ctr >= cate_geo_cjv.ctr
+ORDER BY t.cate_pid, t.ctr DESC
+LIMIT 1000000
     "
 
     local sql_file=${LOCAL_LOG}"/${TIME_TAG}.sql"
@@ -95,9 +124,15 @@ GROUP BY cate_pid
         exit 1
     fi
 
+    echo "process_to_kv_res"
+    ${PYTHON_BIN} ./process_to_kv_res.py \
+        --input ${merged_file} \
+        --output ${kv_file} \
+        --max_size_in_key ${MAX_DOC_IN_GEO}
+
     echo "start write to redis"
     ${PYTHON_BIN} ./write_redis.py \
-        --input ${merged_file} \
+        --input ${kv_file} \
         --prefix ${REDIS_PREFIX} \
         --ttl ${REDIS_TTL_SEC}
     return $?
